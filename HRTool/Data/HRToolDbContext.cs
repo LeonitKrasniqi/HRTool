87d2816ddc4073f7bcfdb645616a7734faf3839c
LeonitKrasniqi <leonitkr88@gmail.com>
2024-06-08 22:36:11 +0200

    Added a connection with database. Added DbContext Class for this project.

---
+++ HRTool/Data/HRToolDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HRTool.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRTool.Data
+{
+    public class HRToolDbContext(DbContextOptions<HRToolDbContext> hrToolDbContext) : DbContext(hrToolDbContext)
+    {
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+}
