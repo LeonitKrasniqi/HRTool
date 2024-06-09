af53316127b0bab6bedb1765be044f4c44a6d684
LeonitKrasniqi <leonitkr88@gmail.com>
2024-06-09 15:33:06 +0200

    Implemented CRUD for User Entity. Added Interface and Class logic.

---
+++ HRTool/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using HRTool.Models.Entities;
+
+namespace HRTool.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateUserAsync(User user);
+        Task<List<User>> GetAllAsync();
+        Task<User> GetByIdAsync(Guid id);
+        Task<User> UpdateAsync(User user);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
---
+++ HRTool/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using HRTool.Models.Entities;
+
+namespace HRTool.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> CreateUserAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
